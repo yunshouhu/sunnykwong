05109585f172cc38f7b693bfc661b90ad3d06712
skwong@sunnykwong.com <skwong@sunnykwong.com@3c733f50-8780-0929-1c2b-a13d2c7d3b54>
2010-10-18 14:54:07 +0000

    Free Edition.

---
+++ OMCFree/src/com/sunnykwong/omc/OMCCreditsActivity.java
@@ -0,0 +1,17 @@
+package com.sunnykwong.omc;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class OMCCreditsActivity extends PreferenceActivity { 
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+    	super.onCreate(savedInstanceState);
+        
+       	addPreferencesFromResource(R.xml.credits);
+
+    }
+
+} 
