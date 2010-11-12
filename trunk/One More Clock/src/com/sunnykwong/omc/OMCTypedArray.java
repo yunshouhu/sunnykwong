a63abe9975495a9aa9de831aaf6787e247881de8
skwong@sunnykwong.com <skwong@sunnykwong.com@3c733f50-8780-0929-1c2b-a13d2c7d3b54>
2010-11-12 01:38:02 +0000

    v1.1 updates. - WIP: Nesting tags.

--- trunk/One More Clock/src/com/sunnykwong/omc/OMCTypedArray.java
+++ trunk/One More Clock/src/com/sunnykwong/omc/OMCTypedArray.java
@@ -39,12 +39,171 @@ public class OMCTypedArray  {
 	static public String[] findTokens(String[] sArray, int aWI) {
 		String[] result = new String[sArray.length];
 		for (int i = 0; i < sArray.length; i++) {
-			result[i] = OMCTypedArray.resolveTokens(sArray[i], aWI, false);
+			result[i] = OMCTypedArray.resolveTokens(sArray[i], aWI);
 		}
 		return result;
 	}
+	
+	static public String resolveOneToken(String sRawString, int aWI) {
+		String sBuffer;
+		//by default, return strftime'd string.
+		if (OMC.PREFS.getBoolean("widget24HrClock"+aWI, true)) {
+			sBuffer = (OMC.TIME.format(
+				OMC.PREFS.getBoolean("widgetLeadingZero", true)? 
+					sRawString : sRawString.replaceAll("%H", "%k")
+				)
+			);
+		} else {
+			sBuffer = (OMC.TIME.format(
+				OMC.PREFS.getBoolean("widgetLeadingZero", true)? 
+					sRawString.replaceAll("%I", "%k") : sRawString.replaceAll("%H", "%l")
+				)
+			);
+		}
+		
+		String result = sBuffer;
+		
+		// If it is actually a dynamic element, resolve it
 
-	static public String resolveTokens(String sRawString, int aWI, boolean isDirective) {
+		StringTokenizer st = new StringTokenizer(sBuffer, "_");
+
+		//Get the first element (command).
+		String sToken = st.nextToken();
+		if (sToken.startsWith("shift")) {
+
+			// value that changes linearly and repeats every X seconds from 6am.
+			String sType = st.nextToken();
+			int iIntervalSeconds =  Integer.parseInt(st.nextToken());
+			int iGradientSeconds = 0;
+
+			// Where are we in time?  intGradientSeconds starts the count from either 12am or 6am
+			if (sToken.equals("shift12")) {
+				iGradientSeconds = OMC.TIME.second + OMC.TIME.minute*60 + OMC.TIME.hour*3600;
+			} else { 
+				// (sToken.equals("shift6"))
+				iGradientSeconds = OMC.TIME.second + OMC.TIME.minute*60 + ((OMC.TIME.hour+18)%24)*3600;
+			} 
+
+			float gradient = (iGradientSeconds % iIntervalSeconds)/(float)iIntervalSeconds;
+			
+			if (sType.equals("number")) {
+				result = String.valueOf(OMCTypedArray.numberInterpolate(
+						Integer.parseInt(st.nextToken()), 
+						Integer.parseInt(st.nextToken()), 
+						Integer.parseInt(st.nextToken()), 
+						gradient));
+			} else if (sType.equals("color")) { // must be color
+				int color = OMCTypedArray.colorInterpolate(
+						st.nextToken(), 
+						st.nextToken(), 
+						st.nextToken(), 
+						gradient);
+				result = String.valueOf("#" + 								
+						Integer.toHexString(Color.alpha(color)) +
+						Integer.toHexString(Color.red(color)) +
+						Integer.toHexString(Color.green(color)) +
+						Integer.toHexString(Color.blue(color))
+						);
+			} else {
+				//Unknown - do nothing
+			}
+
+		} else if (sToken.equals("ap24")) { // must be color
+			if (OMC.PREFS.getBoolean("widget24HrClock"+aWI, true)) {
+				st.nextToken();
+				st.nextToken();
+				result = (st.nextToken());
+			} else if (OMC.TIME.hour < 12) {
+				result = (st.nextToken());
+			} else {
+				st.nextToken();
+				result = (st.nextToken());
+			}
+		} else if (sToken.equals("fullenglishtime")){
+			// full english time
+			String sType = st.nextToken();
+			String sTemp = "";
+			if (OMC.TIME.minute == 0) {
+				sTemp = OMC.WORDNUMBERS[Integer.parseInt(OMC.TIME.format("%I"))] + " o'Clock.";
+			} else if (OMC.TIME.minute == 30) {
+				sTemp = "** half past " + OMC.WORDNUMBERS[Integer.parseInt(OMC.TIME.format("%I"))] + "."; 
+			} else if (OMC.TIME.minute == 15) {
+				sTemp = "A Quarter past " + OMC.WORDNUMBERS[Integer.parseInt(OMC.TIME.format("%I"))] + "."; 
+			} else if (OMC.TIME.minute == 45) {
+				if (OMC.TIME.hour == 11 || OMC.TIME.hour == 23) {
+					sTemp = "A Quarter to Twelve.";
+				} else {
+					sTemp = "A Quarter to " 
+					+ OMC.WORDNUMBERS[Integer.parseInt(OMC.TIME.format("%I"))+1] + ".";
+				}
+			} else if (OMC.TIME.minute > 30) {
+				if (OMC.TIME.hour == 11 || OMC.TIME.hour == 23) {
+					sTemp = OMC.WORDNUMBERS[60-Integer.parseInt(OMC.TIME.format("%M"))] + " to Twelve.";
+				} else {
+					sTemp = OMC.WORDNUMBERS[60-Integer.parseInt(OMC.TIME.format("%M"))] + " to " 
+					+ OMC.WORDNUMBERS[Integer.parseInt(OMC.TIME.format("%I"))+1] + ".";
+				}
+			} else {
+				sTemp = OMC.WORDNUMBERS[Integer.parseInt(OMC.TIME.format("%M"))] + " past " 
+				+ OMC.WORDNUMBERS[Integer.parseInt(OMC.TIME.format("%I"))] + ".";
+			}
+			if (sType.equals("diary")) result = (sTemp);
+			else if (sType.equals("upper")) result = (sTemp.toUpperCase());
+			else if (sType.equals("lower")) result = (sTemp.toLowerCase());
+			else result = (sTemp);
+
+		} else if (sToken.equals("digit")) { // must be color
+			String sTemp = st.nextToken();
+
+    		int iOffset = Integer.parseInt(st.nextToken());
+    		result = (sTemp.substring(iOffset-1,iOffset));
+			
+			
+		} else if (sToken.equals("day")){
+			// value that switches between two fixed symbols - day (6a-6p) and night (6p-6a).
+			if (OMC.TIME.hour >= 6 && OMC.TIME.hour < 18) {
+				//Day
+				result = (st.nextToken());
+			} else {
+				//Night - throw away the day token + the night indicator
+				st.nextToken();
+				st.nextToken();
+				result = (st.nextToken());
+			}
+		} else if (sToken.equals("random")){
+			// value that randomly jumps between two values.
+			String sType = st.nextToken();
+			float gradient = OMC.RND.nextFloat();
+			if (sType.equals("number")) {
+				result = String.valueOf(OMCTypedArray.numberInterpolate(
+						Integer.parseInt(st.nextToken()), 
+						Integer.parseInt(st.nextToken()), 
+						gradient));
+			} else if (sType.equals("color")) { // must be color
+				int color = OMCTypedArray.colorInterpolate(
+						st.nextToken(), 
+						st.nextToken(), 
+						gradient);
+				result = ("#" + 								
+						Integer.toHexString(Color.alpha(color)) +
+						Integer.toHexString(Color.red(color)) +
+						Integer.toHexString(Color.green(color)) +
+						Integer.toHexString(Color.blue(color))
+						);
+			} else {
+				//Unknown - do nothing
+			}
+			
+
+		} else {
+			//unrecognized macro - ignore
+		}
+		
+		return result;
+		
+	}
+
+	static public String resolveTokens(String sRawString, int aWI) {
 		System.out.println("Parsing " + sRawString);
 
 		StringBuilder result = new StringBuilder();
@@ -85,7 +244,7 @@ public class OMCTypedArray  {
 					// No more start markers found, but we have an end marker.
 					// Dive into this substring.
 					Log.i("OMCArray","Detected directive: " + sRawString.substring(iCursor+2, iMarker2));
-					result.append(OMCTypedArray.resolveTokens(sRawString.substring(iCursor+2, iMarker2), aWI, true));
+					result.append(OMCTypedArray.resolveOneToken(sRawString.substring(iCursor+2, iMarker2), aWI));
 				} else if (iMarker1 < iMarker2) {
 					//if [% is closer, keep going until we find the innermost [%
 					Log.i("OMCArray","Found nested. ");
@@ -96,208 +255,20 @@ public class OMCTypedArray  {
 					//if %] is closer, we have an end marker.
 					//Dive into this substring.
 					Log.i("OMCArray","Detected directive: " + sRawString.substring(iCursor+2, iMarker2));
-					result.append(OMCTypedArray.resolveTokens(sRawString.substring(iCursor+2, iMarker2), aWI, true));
+					result.append(OMCTypedArray.resolveOneToken(sRawString.substring(iCursor+2, iMarker2), aWI));
 					Log.i("OMCArray","Sending down: " + sRawString.substring(iCursor+2, iMarker2));
 					Log.i("OMCArray","Now at: " + result.toString());
 					//Start looking for the next directive.
 					iCursor = iMarker2+2;
 				}
 				} while (iMarker1 != -1);
-				return result.toString();
-
-				//				do {
-//					System.out.println("Markers: " +iCursor+" " + iMarker1+ " " + iMarker2);
-//					result.append(sRawString.substring(
-//							iMarker1,sRawString.indexOf(
-//								"[%",iMarker1)==-1? 
-//								sRawString.length() : 
-//								sRawString.indexOf("[%",iMarker1)
-//						)
-//					);
-//					System.out.println("Appended. Now at " + result.toString());
-//					iMarker1 = sRawString.indexOf("[%",iMarker1);
-//				} while (iMarker1 < iMarker2 && iMarker1 != -1);
-//				System.out.println("Next Marker: " +iCursor+" " + iMarker1+ " " + iMarker2);
-//				System.out.println("Now at " + result.toString());
-
-				// if iMarker is at the end of the string, bad parsing occurred.
-				// exit gracefully (try to)
-//				if (iMarker1 >= sRawString.length() || iMarker1 == -1) {
-//					break;
-//				}
-////				int iMarker2 = sRawString.indexOf("%]",iMark+1);
-//				System.out.println(sRawString + iMarker1 + " to " + iMarker2);
-//				result.append(OMCTypedArray.resolveTokens(sRawString.substring(iMarker1,iMarker2), aWI, true));
-//				iCursor = iMarker2+2;
-			}
-			return result.toString();
-		}
-
-		// If token is fully resolved (no dynamic elements), 
-		// Then we either return the string whole 
-		//(This lets Time.format do its magic strftime style,
-		// Also has the nice effect of letting the %values
-		// resolve first before applying our dynamic attributes.
-		if (!isDirective) {
-    		if (OMC.PREFS.getBoolean("widget24HrClock"+aWI, true)) {
-    			result.append(OMC.TIME.format(
-    				OMC.PREFS.getBoolean("widgetLeadingZero", true)? 
-    					sRawString : sRawString.replaceAll("%H", "%k")
-    				)
-    			);
-    		} else {
-    			result.append(OMC.TIME.format(
-    				OMC.PREFS.getBoolean("widgetLeadingZero", true)? 
-    					sRawString.replaceAll("%I", "%k") : sRawString.replaceAll("%H", "%l")
-    				)
-    			);
-    		}
-
-		// Or if it is actually a dynamic element, resolve it
-		} else {
-		
-			StringTokenizer st = new StringTokenizer(sRawString, "_");
-	
-			//Get the first element (command).
-			String sToken = st.nextToken();
-			if (sToken.startsWith("shift")) {
-	
-				// value that changes linearly and repeats every X seconds from 6am.
-				String sType = st.nextToken();
-				int iIntervalSeconds =  Integer.parseInt(st.nextToken());
-				int iGradientSeconds = 0;
-	
-				// Where are we in time?  intGradientSeconds starts the count from either 12am or 6am
-				if (sToken.equals("shift12")) {
-					iGradientSeconds = OMC.TIME.second + OMC.TIME.minute*60 + OMC.TIME.hour*3600;
-				} else { 
-					// (sToken.equals("shift6"))
-					iGradientSeconds = OMC.TIME.second + OMC.TIME.minute*60 + ((OMC.TIME.hour+18)%24)*3600;
-				} 
-	
-				float gradient = (iGradientSeconds % iIntervalSeconds)/(float)iIntervalSeconds;
 				
-				if (sType.equals("number")) {
-					result.append(OMCTypedArray.numberInterpolate(
-							Integer.parseInt(st.nextToken()), 
-							Integer.parseInt(st.nextToken()), 
-							Integer.parseInt(st.nextToken()), 
-							gradient));
-				} else if (sType.equals("color")) { // must be color
-					int color = OMCTypedArray.colorInterpolate(
-							st.nextToken(), 
-							st.nextToken(), 
-							st.nextToken(), 
-							gradient);
-					result.append("#" + 								
-							Integer.toHexString(Color.alpha(color)) +
-							Integer.toHexString(Color.red(color)) +
-							Integer.toHexString(Color.green(color)) +
-							Integer.toHexString(Color.blue(color))
-							);
-				} else {
-					//Unknown - do nothing
-				}
-	
-			} else if (sToken.equals("ap24")) { // must be color
-				if (OMC.PREFS.getBoolean("widget24HrClock"+aWI, true)) {
-					st.nextToken();
-					st.nextToken();
-					result.append(st.nextToken());
-				} else if (OMC.TIME.hour < 12) {
-					result.append(st.nextToken());
-				} else {
-					st.nextToken();
-					result.append(st.nextToken());
-				}
-			} else if (sToken.equals("fullenglishtime")){
-				// full english time
-				String sType = st.nextToken();
-				String sTemp = "";
-				if (OMC.TIME.minute == 0) {
-					sTemp = OMC.WORDNUMBERS[Integer.parseInt(OMC.TIME.format("%I"))] + " o'Clock.";
-				} else if (OMC.TIME.minute == 30) {
-					sTemp = "** half past " + OMC.WORDNUMBERS[Integer.parseInt(OMC.TIME.format("%I"))] + "."; 
-				} else if (OMC.TIME.minute == 15) {
-					sTemp = "A Quarter past " + OMC.WORDNUMBERS[Integer.parseInt(OMC.TIME.format("%I"))] + "."; 
-				} else if (OMC.TIME.minute == 45) {
-					if (OMC.TIME.hour == 11 || OMC.TIME.hour == 23) {
-						sTemp = "A Quarter to Twelve.";
-					} else {
-						sTemp = "A Quarter to " 
-						+ OMC.WORDNUMBERS[Integer.parseInt(OMC.TIME.format("%I"))+1] + ".";
-					}
-				} else if (OMC.TIME.minute > 30) {
-					if (OMC.TIME.hour == 11 || OMC.TIME.hour == 23) {
-						sTemp = OMC.WORDNUMBERS[60-Integer.parseInt(OMC.TIME.format("%M"))] + " to Twelve.";
-					} else {
-						sTemp = OMC.WORDNUMBERS[60-Integer.parseInt(OMC.TIME.format("%M"))] + " to " 
-						+ OMC.WORDNUMBERS[Integer.parseInt(OMC.TIME.format("%I"))+1] + ".";
-					}
-				} else {
-					sTemp = OMC.WORDNUMBERS[Integer.parseInt(OMC.TIME.format("%M"))] + " past " 
-					+ OMC.WORDNUMBERS[Integer.parseInt(OMC.TIME.format("%I"))] + ".";
-				}
-				if (sType.equals("diary")) result.append(sTemp);
-				else if (sType.equals("upper")) result.append(sTemp.toUpperCase());
-				else if (sType.equals("lower")) result.append(sTemp.toLowerCase());
-				else result.append(sTemp);
-	
-			} else if (sToken.equals("digit")) { // must be color
-				String sTemp;
-	
-	    		if (OMC.PREFS.getBoolean("widget24HrClock"+aWI, true)) {
-	    			sTemp = OMC.PREFS.getBoolean("widgetLeadingZero", true)? "%H" : "%k";
-	    		} else {
-	    			sTemp = OMC.PREFS.getBoolean("widgetLeadingZero", true)? "%I" : "%l";
-	    		}
-	    		sTemp = OMC.TIME.format(st.nextToken().replace("%H", sTemp));
-	    		int iOffset = Integer.parseInt(st.nextToken());
-	    		result.append(sTemp.substring(iOffset-1,iOffset));
-				
-				
-			} else if (sToken.equals("day")){
-				// value that switches between two fixed symbols - day (6a-6p) and night (6p-6a).
-				if (OMC.TIME.hour >= 6 && OMC.TIME.hour < 18) {
-					//Day
-					result.append(st.nextToken());
-				} else {
-					//Night - throw away the day token + the night indicator
-					st.nextToken();
-					st.nextToken();
-					result.append(st.nextToken());
-				}
-			} else if (sToken.equals("random")){
-				// value that randomly jumps between two values.
-				String sType = st.nextToken();
-				float gradient = OMC.RND.nextFloat();
-				if (sType.equals("number")) {
-					result.append(OMCTypedArray.numberInterpolate(
-							Integer.parseInt(st.nextToken()), 
-							Integer.parseInt(st.nextToken()), 
-							gradient));
-				} else if (sType.equals("color")) { // must be color
-					int color = OMCTypedArray.colorInterpolate(
-							st.nextToken(), 
-							st.nextToken(), 
-							gradient);
-					result.append("#" + 								
-							Integer.toHexString(Color.alpha(color)) +
-							Integer.toHexString(Color.red(color)) +
-							Integer.toHexString(Color.green(color)) +
-							Integer.toHexString(Color.blue(color))
-							);
-				} else {
-					//Unknown - do nothing
-				}
-				
-	
-			} else {
-				//unrecognized macro - ignore
+				// Pass it up to resolve nested stuff.
+				return OMCTypedArray.resolveTokens(result.toString(), aWI);
 			}
 		}
-
-		return result.toString();
+		// If no tags found, then just return the raw string; we're done! 
+		return sRawString;
 
 	}
 	
