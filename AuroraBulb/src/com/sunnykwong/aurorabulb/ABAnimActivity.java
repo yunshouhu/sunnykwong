034c0f381de32b74fdabba6e8088ddc95f9c23a8
skwong@sunnykwong.com <skwong@sunnykwong.com@3c733f50-8780-0929-1c2b-a13d2c7d3b54>
2010-12-06 23:29:16 +0000

    v1.0 Aurorabulb second working version.

--- AuroraBulb/src/com/sunnykwong/aurorabulb/ABAnimActivity.java
+++ AuroraBulb/src/com/sunnykwong/aurorabulb/ABAnimActivity.java
@@ -15,6 +15,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.graphics.Matrix;
+import android.graphics.Paint.Align;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -35,23 +36,29 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Collections;
 import java.util.Iterator;
+
 import android.view.Display;
 
 public class ABAnimActivity extends Activity {
 
-	Matrix mMatrix;
 	boolean mDone;
 	Handler mHandler;
 	ImageView mScrn;
 	List<Bitmap> mImagebuffer;
 	List<Bitmap> mBitmapBin;
 	int[] mSliver;
-	long lastUpdateTime = 0l;
+	long startTime = 0l;
 	long thisUpdateTime = 0l;
 	long nextFrameTime = 0l;
 	
+	final Runnable mAnim = new Runnable() {
+		public void run() {
+			renderFrames();
+		}
+	};
 	final Runnable mFlip = new Runnable() {
 		public void run() {
+			thisUpdateTime = System.currentTimeMillis();
 			mScrn.invalidate();
 		}
 	};
@@ -65,57 +72,92 @@ public class ABAnimActivity extends Activity {
 		mScrn = (ImageView)findViewById(R.id.ScreenImage);
 		mHandler = new Handler();
 		mDone = false;
+		AB.PT1.setTextAlign(Paint.Align.CENTER);
+		AB.PT1.setTextScaleX(1f);
 
 		AB.SCRNWIDTH = getWindowManager().getDefaultDisplay().getWidth();
 		AB.SCRNHEIGHT = getWindowManager().getDefaultDisplay().getHeight();
 		
-		AB.SRCBUFFER = BitmapFactory.decodeResource(getResources(), R.drawable.llpreview);
+		if (AB.SRCBUFFER== null)AB.SRCBUFFER = BitmapFactory.decodeResource(getResources(), R.drawable.llpreview);
 
-		AB.ROLLBUFFER = Bitmap.createBitmap(240,320,Bitmap.Config.ARGB_4444);
+		AB.ROLLBUFFER = Bitmap.createBitmap(AB.BUFFERWIDTH,AB.BUFFERHEIGHT,Bitmap.Config.ARGB_4444);
 		AB.ROLLCANVAS = new Canvas(AB.ROLLBUFFER);
 		AB.ROLLCANVAS.setDensity(DisplayMetrics.DENSITY_HIGH);
 
-		mMatrix = new Matrix();
-		mMatrix.postScale((float)AB.SCRNWIDTH, (float)AB.ROLLBUFFER.getHeight()/AB.SRCBUFFER.getHeight());
+		AB.TEMPMATRIX.reset();
+		AB.TEMPMATRIX.postScale((float)AB.SCRNWIDTH, (float)AB.ROLLBUFFER.getHeight()/AB.SRCBUFFER.getHeight());
 		
-		AB.PT1 = new Paint();
-
 		mScrn.setImageBitmap(AB.ROLLBUFFER);
 		mScrn.buildDrawingCache();
 
-		renderFrames();
+		countDown();
 	}
-	
+	public void countDown() {
+		// Begin animation; use new thread for max precision
+		Thread t = new Thread() {
+			public void run() {
+				// Countdown
+				nextFrameTime = System.currentTimeMillis();
+				for (int i=AB.COUNTDOWNSECONDS; i>0; i-- ) {
+					nextFrameTime += 1000l;
+		
+					AB.ROLLCANVAS.drawColor(Color.BLACK);
+					AB.ROLLCANVAS.drawText(String.valueOf(i), AB.BUFFERWIDTH/2, 2*AB.BUFFERHEIGHT/3, AB.PT1);
+					mHandler.post(mFlip);
+					
+					while (System.currentTimeMillis() < nextFrameTime) {
+						try {
+							Thread.sleep(2);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+		
+				}
+				mHandler.post(mAnim);
+			}
+		};		
+		t.start();
+
+	}
 	public void renderFrames() {
 
+		// Begin animation; use new thread for max precision
 		Thread t = new Thread() {
 			public void run() {
 				Bitmap bmpTemp;
+				startTime = System.currentTimeMillis();
 				nextFrameTime = System.currentTimeMillis();
 				for (int i=0; i<AB.SRCBUFFER.getWidth(); i++ ) {
-					nextFrameTime += 50; //20fps
+					nextFrameTime += 1000l/AB.TARGETFPS;
+
+					AB.ROLLCANVAS.drawColor(Color.BLACK);
 					bmpTemp = Bitmap.createBitmap(AB.SRCBUFFER, i, 0, 1, AB.SRCBUFFER.getHeight());
 					bmpTemp.prepareToDraw();
-					AB.ROLLCANVAS.drawBitmap(bmpTemp, mMatrix, AB.PT1);
+					AB.ROLLCANVAS.drawBitmap(bmpTemp, AB.TEMPMATRIX, AB.PT1);
 
 					while (System.currentTimeMillis() < nextFrameTime) {
 						try {
-							Thread.sleep(3);
+							Thread.sleep(2);
 						} catch (InterruptedException e) {
 							e.printStackTrace();
 						}
 					}
 					
-					
-					thisUpdateTime = System.currentTimeMillis();
-					System.out.println("FPS: " + 1000f/(float)(thisUpdateTime-lastUpdateTime));
 					mHandler.post(mFlip);
-					lastUpdateTime = thisUpdateTime;
+
+					if (AB.DEBUG) Log.i("ABAnim","FPS: " + (i+1)*1000f/(float)(thisUpdateTime-startTime));
 
 				}
 				AB.ROLLCANVAS.drawColor(Color.BLACK);
 				mHandler.post(mFlip);
-				
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				setResult(Activity.RESULT_OK);
+				finish();
 				
 			}
 		};
--- AuroraBulb/src/com/sunnykwong/aurorabulb/ABPrefActivity.java
+++ AuroraBulb/src/com/sunnykwong/aurorabulb/ABPrefActivity.java
@@ -1,33 +1,65 @@
 package com.sunnykwong.aurorabulb;
 
+import android.graphics.Canvas;
 import android.app.Activity;
 import android.os.Bundle;
 import android.app.AlertDialog;
 import android.content.ComponentName;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.DialogInterface.OnKeyListener;
 import android.content.pm.PackageManager;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
+import android.preference.PreferenceCategory;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.Log;
 import android.widget.Toast;
+import java.io.StringWriter;
+import java.io.PrintWriter;
 
 
 public class ABPrefActivity extends PreferenceActivity implements OnPreferenceChangeListener {
-
 	AlertDialog mAD;
+	PreferenceCategory bitmapstuff, textstuff;
+	PreferenceScreen toplevel;
+	StringWriter sw = new StringWriter(10);;
     
 	@Override
     public void onCreate(Bundle savedInstanceState) {
 
     	super.onCreate(savedInstanceState);
-        
+
+		this.getPreferenceManager().setSharedPreferencesName(AB.PREFNAME);
+    	this.addPreferencesFromResource(R.xml.abprefs);
+    	
+    
+    	toplevel = (PreferenceScreen)findPreference("rootPrefs");
+    	bitmapstuff = (PreferenceCategory)findPreference("bitmapstuff");
+    	textstuff = (PreferenceCategory)findPreference("textstuff");
+    	
+    	new PrintWriter(sw).format("#%x", AB.PREFS.getInt("textColor", 0));
+    	
+		findPreference("pickColor").setSummary("Picked: " + sw.toString());
+		sw.getBuffer().setLength(0);
+		findPreference("pickText").setSummary(AB.PREFS.getString("pickText", "Aurora Bulb"));
+		
+    	
+    	toplevel.removePreference(bitmapstuff);
+
+    	findPreference("pickColor").setOnPreferenceChangeListener(this);
+    	findPreference("timeShutterDuration").setOnPreferenceChangeListener(this);
+    	findPreference("timePhotoTimer").setOnPreferenceChangeListener(this);
+
     	mAD = new AlertDialog.Builder(this)
-    		.setTitle("DroidExpose!")
-    		.setMessage("Thanks for downloading!\nTo begin, hit the back button to go back to the home screen, then push the menu button, select 'Add', then 'Widgets' to see 'One More Clock' listed.  Have fun!")
+    		.setTitle("Aurora Bulb!")
+    		.setMessage("Thanks for downloading!\nJust hit Go! for some quick fun, or change the text/colors to suit your whim.  Have fun!")
     	    .setCancelable(true)
     	    .setOnKeyListener(new OnKeyListener() {
     	    	public boolean onKey(DialogInterface arg0, int arg1, android.view.KeyEvent arg2) {
@@ -39,70 +71,136 @@ public class ABPrefActivity extends PreferenceActivity implements OnPreferenceCh
 
     }
 
-    // If user sets a seeded theme, set external to false
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-
-//        	if (preference == findPreference("widgetTheme")) {
-//    	    	if (DE.DEBUG) Log.i("OMCPref","Setting External to false");
-//    			DE.PREFS.edit().putBoolean("external", false).commit();
-//    	    	return true;
-//        	}
-    	
-    	return false;
+    	if (preference == findPreference("pickColor")) {
+    		try {
+    			int colr = Color.parseColor((String)newValue);
+    			AB.PREFS.edit().putInt("textColor", colr).commit();
+    			new PrintWriter(sw).format("#%x", colr);
+        		findPreference("pickColor").setSummary("Picked: " + sw.toString());
+        		sw.getBuffer().setLength(0);
+    			return true;
+    		} catch (IllegalArgumentException e) {
+    			Toast.makeText(this, "Invalid Color!", Toast.LENGTH_SHORT).show();
+    			return false;
+    		}
+    	} else if (preference == findPreference("timeShutterDuration")) {
+    		int temp;
+    		try {
+    			temp = Integer.parseInt((String)newValue);
+    		} catch (IllegalArgumentException e) {
+    			Toast.makeText(this, "Invalid Shutter Duration!", Toast.LENGTH_SHORT).show();
+    			return false;
+    		}
+    		if (temp < 10) {
+    			Toast.makeText(this, "Invalid Shutter Duration!", Toast.LENGTH_SHORT).show();
+    			return false;
+    		}
+    		findPreference("timeShutterDuration").setSummary("Shutter at (seconds): " + (String)newValue);
+			return true;
+    	} else if (preference == findPreference("timePhotoTimer")) {
+    		int temp;
+    		try {
+    			temp = Integer.parseInt((String)newValue);
+    		} catch (IllegalArgumentException e) {
+    			Toast.makeText(this, "Invalid Photo Timer Duration!", Toast.LENGTH_SHORT).show();
+    			return false;
+    		}
+    		if (temp < 0) {
+    			Toast.makeText(this, "Invalid Photo Timer Duration!", Toast.LENGTH_SHORT).show();
+    			return false;
+    		}
+    		findPreference("timePhotoTimer").setSummary("Assuming Cam timer of (seconds): " + (String)newValue);
+			return true;
+    	}
+    	return true;
     }
-    
+
     // If user clicks on a preference...
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
     		Preference preference) {
-//        	if (preference == getPreferenceScreen().findPreference("widgetPrefs") && DE.FREEEDITION) {
-//            	OMCPrefActivity.mAD = new AlertDialog.Builder(this)
-//    			.setCancelable(true)
-//    			.setTitle("Why are the widgets so big?")
-//    			.setMessage("Actually, OMC offers widget sizes of 4x2, 4x1, 3x1 and 2x1.\nPlease consider upgrading to the full edition of OMC to get these sizes!\nAlternatively, if you use an alternative launcher such as ADW or Launcher Pro, you should be able to approximate this ability by dynamically resizing the 4x2 widget.")
-//    			.setPositiveButton("Take me to the paid version!", new DialogInterface.OnClickListener() {
-//    					
-//    					@Override
-//    					public void onClick(DialogInterface dialog, int which) {
-//    						// TODO Auto-generated method stub
-//    						OMCPrefActivity.mAD.dismiss();
-//    						OMCPrefActivity.this.startActivity(DE.OMCMARKETINTENT);
-//    						OMCPrefActivity.this.finish();
-//    						
-//    					}
-//    				}).create();
-//            	OMCPrefActivity.mAD.show();
-//        	}
+    	if (preference == getPreferenceScreen().findPreference("go")) {
+    		prepGlobalVars();
+        	this.startActivityForResult(new Intent(this,ABAnimActivity.class),0);
+    	}
+    	if (preference == findPreference("showWhat")) {
+    		if (AB.PREFS.getString("showWhat","bitmap").equals("bitmap")) {
+    			AB.PREFS.edit().putString("showWhat", "text").commit();
+    			findPreference("showWhat").setTitle("Aurora Bulb Renders Text");
+        		toplevel.removePreference(bitmapstuff);
+        		toplevel.addPreference(textstuff);
+    		} else {
+    			AB.PREFS.edit().putString("showWhat", "bitmap").commit();
+    			findPreference("showWhat").setTitle("Aurora Bulb Renders Bitmap");
+        		toplevel.removePreference(textstuff);
+        		toplevel.addPreference(bitmapstuff);
+    		}
+    	}
+    	if (preference == findPreference("pickBitmap")) {
+            Intent intentBrowseFiles = new
+            Intent(Intent.ACTION_GET_CONTENT);
+                            intentBrowseFiles.setType("image/*");
+
+            intentBrowseFiles.addCategory(Intent.CATEGORY_OPENABLE);     		
+            startActivityForResult(intentBrowseFiles, 1);
+    	}
     	return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
     // The result is obtained in onActivityResult:
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		getPreferenceScreen().setEnabled(true);
-		// If it's an independent child activity, do nothing
-		if (requestCode == 0) return;
-		if (data != null) {
-			String s = data.toUri(MODE_PRIVATE).toString();
-			AB.PREFS.edit().putString("URI", s).commit();
+		if (requestCode == 0 ) { // From anim
+			getPreferenceScreen().setEnabled(true);
+			// If it's an independent child activity, do nothing
+			return;
+		} else if (requestCode == 1) {  // From pick Bitmap
+			if (data!=null) {
+				findPreference("pickBitmap").setSummary("Selected " + data.getDataString());
+			}
 		}
-	}
+}
 
     
     public void dialogCancelled() {
-//       	if (OMCPrefActivity.mAD!=null) { // && mAD.isShowing()
-//       		OMCPrefActivity.mAD.dismiss();
-//       		OMCPrefActivity.mAD = null;
-//       	}
     	mAD.cancel();
-    	this.startActivity(new Intent(this,ABAnimActivity.class));
-    	finish();
+    	return;
+    }
+    
+    public void prepGlobalVars() {
+    	//ok, set global vars before we pass control to anim.
+    	AB.COUNTDOWNSECONDS = Integer.parseInt(AB.PREFS.getString("timePhotoTimer", "10"));
+    	System.out.println(AB.PREFS.getInt("textColor", Color.WHITE));
+    	AB.PT1.setColor(AB.PREFS.getInt("textColor", Color.WHITE));
+		AB.PT1.setTextAlign(Paint.Align.LEFT);
+		int textwidth = (int)AB.PT1.measureText(AB.PREFS.getString("pickText", "Aurora Bulb"));
+		int shutterDuration = Integer.parseInt(AB.PREFS.getString("timeShutterDuration", "10"));
+		//Since we can only manage 20fps, we will have to squeeze the text.
+		//How many lines can we manage over the shutter duration?
+		int bufferwidth = shutterDuration * 20;
+		AB.PT1.setTextScaleX((float)bufferwidth/textwidth);
+		
+    	AB.SRCBUFFER = Bitmap.createBitmap((int)AB.PT1.measureText(AB.PREFS.getString("pickText", "Aurora Bulb")), AB.BUFFERHEIGHT, Bitmap.Config.ARGB_4444);
+    	System.out.println(AB.PT1.measureText(AB.PREFS.getString("pickText", "Aurora Bulb")));
+    	AB.SRCCANVAS = new Canvas(AB.SRCBUFFER);
+    	AB.SRCCANVAS.drawText(AB.PREFS.getString("pickText", "Aurora Bulb"), 0, AB.SRCBUFFER.getHeight()/2, AB.PT1);
     }
 
-    @Override
-    public void onPause() {
-    	super.onPause();
-    }
+
+//   @Override
+//    protected void onResume() {
+//        super.onResume();
+//        // Set up a listener whenever a key changes            
+//        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        // Unregister the listener whenever a key changes            
+//        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);    
+//    }
 
     @Override
     public void onDestroy() {
