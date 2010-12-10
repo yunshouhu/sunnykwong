cfa6dfa53a559164f72f58bcb06263ce937b915b
skwong@sunnykwong.com <skwong@sunnykwong.com@3c733f50-8780-0929-1c2b-a13d2c7d3b54>
2010-12-10 20:32:19 +0000

    v1.1.6 - Singleton support.

--- trunk/One More Clock/src/com/sunnykwong/omc/OMC.java
+++ trunk/One More Clock/src/com/sunnykwong/omc/OMC.java
@@ -293,19 +293,19 @@ public class OMC extends Application {
 	    	context.getPackageManager()
 			.setComponentEnabledSetting(
 					OMC.WIDGET4x1CNAME,
-					OMC.PREFS.getBoolean("bFourByOne", false) ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+					OMC.PREFS.getBoolean("bFourByOne", true) ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED
 							: PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
 					PackageManager.DONT_KILL_APP);
 	    	context.getPackageManager()
 					.setComponentEnabledSetting(
 							OMC.WIDGET3x1CNAME,
-							OMC.PREFS.getBoolean("bThreeByOne", false) ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+							OMC.PREFS.getBoolean("bThreeByOne", true) ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED
 									: PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
 							PackageManager.DONT_KILL_APP);
 	    	context.getPackageManager()
 			.setComponentEnabledSetting(
 					OMC.WIDGET2x1CNAME,
-					OMC.PREFS.getBoolean("bTwoByOne", false) ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+					OMC.PREFS.getBoolean("bTwoByOne", true) ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED
 							: PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
 					PackageManager.DONT_KILL_APP);
     	}
