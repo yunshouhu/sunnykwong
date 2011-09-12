99b125dfed1d0d7d34caf5641b7d4aa0ab202c99
skwong@sunnykwong.com <skwong@sunnykwong.com@3c733f50-8780-0929-1c2b-a13d2c7d3b54>
2011-09-12 17:29:08 +0000

    v124 RC1.  Fix TTL issue.

--- One More Clock/src/com/sunnykwong/omc/OMC.java
+++ One More Clock/src/com/sunnykwong/omc/OMC.java
@@ -88,7 +88,8 @@ public class OMC extends Application {
 //  NO NEED TO CHANGE BELOW THIS LINE FOR VERSIONING
 	static int faqtoshow = 0;
 	static final String[] FAQS = {
-		"After an extended hiatus, Xaffron presents version 1.2.3!  As of this version, we are up to a dizzying 58 clocks (from 37 in v1.2.2).",
+		"After an extended hiatus, Xaffron presents a new version of OMC!  As of this version, we are up to a dizzying 58 clocks (from 37 in v1.2.2).",
+		"v1.2.4 fixes a showstopper issue with Tap-to-Launch settings not sticking.  Thanks for yelling at me early and often... keep the feedback coming!",
 		"OMC now supports battery levels.  Some of the widgets contain battery indicators in surprising ways - browse around and experiment!",
 		"You can now specify a timezone for each clock independently of the device setting.  This allows for nice home/travel clock scenarios.",
 		"Version 1.2.3 had to move back to 16-bit color rendering due to performance issues.  The good news, however, is that v1.2.3 is much snappier than previous versions!",
@@ -449,7 +450,7 @@ public class OMC extends Application {
 		OMC.PREFS.edit().putString("widgetTheme"+aWI, OMC.PREFS.getString("widgetTheme"+aWI, OMC.PREFS.getString("widgetTheme", OMC.DEFAULTTHEME)))
 		.putBoolean("widget24HrClock"+aWI, OMC.PREFS.getBoolean("widget24HrClock"+aWI, OMC.PREFS.getBoolean("widget24HrClock", true)))
 		.putBoolean("widgetLeadingZero"+aWI, OMC.PREFS.getBoolean("widgetLeadingZero"+aWI, OMC.PREFS.getBoolean("widgetLeadingZero", true)))
-		.putString("URI"+aWI, "")
+		.putString("URI"+aWI, OMC.PREFS.getString("URI"+aWI, ""))
 		.putString("sTimeZone"+aWI, OMC.PREFS.getString("sTimeZone"+aWI, "default"))
 		.commit();
 	}
