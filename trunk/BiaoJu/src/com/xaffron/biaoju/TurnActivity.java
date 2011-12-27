a739f45e818b0b44fbf0f3a7b84bf8b67749ae0a
skwong@sunnykwong.com <skwong@sunnykwong.com@3c733f50-8780-0929-1c2b-a13d2c7d3b54>
2011-12-27 22:32:05 +0000

    BJ Initial import.

--- trunk/BiaoJu/src/com/xaffron/biaoju/TurnActivity.java
+++ trunk/BiaoJu/src/com/xaffron/biaoju/TurnActivity.java
@@ -1,37 +1,49 @@
 package com.xaffron.biaoju;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.widget.ViewFlipper;
 //import android.util.Log;
 //import android.view.Window;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.os.Bundle;
 import android.view.GestureDetector;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
+import android.view.ViewGroup;
 import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.animation.Animation;
 
 public class TurnActivity extends Activity {
 
-	private static boolean DEBUG;
-	private static final int SWIPE_MIN_DISTANCE = 120;
-	private static final int SWIPE_MAX_OFF_PATH = 250;
-	private static final int SWIPE_THRESHOLD_VELOCITY = 200;
-	private GestureDetector gestureDetector;
-	View.OnTouchListener gestureListener;
-	private Animation slideLeftIn;
-	private Animation slideLeftOut;
-	private Animation slideRightIn;
-	private Animation slideRightOut;
+	
+	public static ScreenAdapter SCREENADAPTER;
+	public static boolean DEBUG;
 
-	private Button mActionButton;
-	private TextView mConsoleView;
-	private ViewFlipper mViewFlipper;
+	public Button mActionButton;
+	public TextView mConsoleView, mBlowbyBlow;
+	public View mAction;
+	public Gallery mGallery;
 
 	static GameMaster master;
 	
@@ -42,14 +54,20 @@ public class TurnActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        LayoutInflater inflater = LayoutInflater.from(getApplicationContext());
+		mAction = inflater.inflate(R.layout.action, null);
+        mBlowbyBlow = (TextView)mAction.findViewById(R.id.blowbyblow);
+        
         DEBUG = Boolean.parseBoolean(getString(R.string.DEBUG));
         
-        mViewFlipper = (ViewFlipper)findViewById(R.id.details);
-        mViewFlipper.setDisplayedChild(1);
         mActionButton = (Button)findViewById(R.id.Button01);
         mActionButton.setClickable(false);
         mConsoleView = (TextView)findViewById(R.id.console);
-    	mConsoleView.addTextChangedListener(new TextWatcher() {
+        mGallery = (Gallery)findViewById(R.id.details);
+        if (TurnActivity.SCREENADAPTER==null) TurnActivity.SCREENADAPTER = new ScreenAdapter(mAction); 
+        mGallery.setAdapter(TurnActivity.SCREENADAPTER);
+        mGallery.setSelection(1);
+    	mBlowbyBlow.addTextChangedListener(new TextWatcher() {
 			
 			public void onTextChanged(CharSequence s, int start, int before, int count) {
 				//nothing
@@ -68,27 +86,12 @@ public class TurnActivity extends Activity {
         
     	mActionButton.setText("出發!!");
 
-        slideLeftIn = AnimationUtils.loadAnimation(this, R.anim.slide_left_in);
-        slideLeftOut = AnimationUtils.loadAnimation(this, R.anim.slide_left_out);
-        slideRightIn = AnimationUtils.loadAnimation(this, R.anim.slide_right_in);
-        slideRightOut = AnimationUtils.loadAnimation(this, R.anim.slide_right_out);
-        
-        gestureDetector = new GestureDetector(new MyGestureDetector());
-        gestureListener = new View.OnTouchListener() {
-            public boolean onTouch(View v, MotionEvent event) {
-                if (gestureDetector.onTouchEvent(event)) {
-                    return true;
-                }
-                return false;
-            }
-        };
-
         mActionButton.setOnClickListener(new View.OnClickListener(){
         	public void onClick (View v) {
         		//what to do when clicked
         		GameMaster.mt.cancel();
         		master.nextTurn();
-        	}
+        	} 
         });
         
     	if (DEBUG) writeLog("Gui Setup Complete");
@@ -98,43 +101,49 @@ public class TurnActivity extends Activity {
     	master.nextTurn();
     }
     
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        if (gestureDetector.onTouchEvent(event))
-            return true;
-        else
-        	return false;
-    }
-
-    class MyGestureDetector extends SimpleOnGestureListener {
-        @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-            try {
-                if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
-                    return false;
-                // right to left swipe
-                if(e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY && mViewFlipper.getDisplayedChild() != 2) {
-                	mViewFlipper.setInAnimation(slideLeftIn);
-                	mViewFlipper.setOutAnimation(slideLeftOut);
-                	mViewFlipper.showNext();
-                // left to right swipe
-                }  else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY && mViewFlipper.getDisplayedChild() != 0) {
-                	mViewFlipper.setInAnimation(slideRightIn);
-                	mViewFlipper.setOutAnimation(slideRightOut);
-                	mViewFlipper.showPrevious();
-                }
-            } catch (Exception e) {
-                // nothing
-            }
-            return false;
-        }
-    }
-
     public void writeLog(String comment) {
-    	mConsoleView.append(comment + "\n");
+    	mBlowbyBlow.append(comment + "\n");
 //    	Log.i("XAFFRON",comment);
     }
 
+    public class ScreenAdapter extends BaseAdapter {
+
+    	public View tvConsole;
+    	public View ivImage;
+    	public View ivImage2;
+    	
+
+        public ScreenAdapter(View tv) {
+        	tvConsole = tv;
+        	ivImage = tv;
+        	ivImage2 = tv;
+        }
+
+        public int getCount() {
+            return 3;
+        }
+
+        public Object getItem(int position) {
+            return position;
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+        	switch (position) {
+        	case 0: return ivImage;
+        	case 1: return tvConsole;
+        	default: return ivImage2;
+        	}
+        }
+
+        public void dispose() {
+        	System.gc();
+        }
     
+    }
+   
 }
 
