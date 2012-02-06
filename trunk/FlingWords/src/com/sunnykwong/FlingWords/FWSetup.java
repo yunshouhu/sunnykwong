a1c6dd59d0cd8cae2723769d5ed7ed080df1b4bf
skwong@sunnykwong.com <skwong@sunnykwong.com@3c733f50-8780-0929-1c2b-a13d2c7d3b54>
2012-02-06 16:09:03 +0000

    FW cleanup.

--- trunk/FlingWords/src/com/sunnykwong/FlingWords/FWSetup.java
+++ trunk/FlingWords/src/com/sunnykwong/FlingWords/FWSetup.java
@@ -2,8 +2,6 @@ package com.sunnykwong.FlingWords;
 
 import org.json.JSONException;
 
-import com.android.settings.activities.ColorPickerDialog;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
