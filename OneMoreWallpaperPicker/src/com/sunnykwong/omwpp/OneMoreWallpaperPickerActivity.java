b7facb3d5f94d71cad450517a6f36d547d9d25d3
skwong@sunnykwong.com <skwong@sunnykwong.com@3c733f50-8780-0929-1c2b-a13d2c7d3b54>
2012-03-09 18:27:07 +0000

    OMWPP Initial import.

--- OneMoreWallpaperPicker/src/com/sunnykwong/omwpp/OneMoreWallpaperPickerActivity.java
+++ OneMoreWallpaperPicker/src/com/sunnykwong/omwpp/OneMoreWallpaperPickerActivity.java
@@ -1,12 +1,17 @@
 package com.sunnykwong.omwpp;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONObject;
 
 import android.app.Activity;
@@ -16,10 +21,13 @@ import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -33,8 +41,6 @@ import android.app.WallpaperManager;
 
 public class OneMoreWallpaperPickerActivity extends Activity {
 
-	static public final boolean DEBUG=true;
-	static public File SDROOT;
 	
 	public Gallery gallery;
 	public Button btnApply, btnHelp;
@@ -43,32 +49,66 @@ public class OneMoreWallpaperPickerActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-    	if (DEBUG) Log.i("OMWPA","OnCreate");
+    	if (OMWPP.DEBUG) Log.i("OMWPP","Starting Activity");
         getWindow().setWindowAnimations(android.R.style.Animation_Toast);
         
-        SDROOT = this.getExternalFilesDir(null);
-        SDROOT.mkdirs();
-
 		setResult(Activity.RESULT_CANCELED);
+ 
+		boolean bNeedRefresh=false;;
+		
+		// If the config file is less than 6 months old, let it be
+		System.out.println(System.currentTimeMillis());
+		System.out.println(OMWPP.CONFIGJSON.optLong("lastupdateepoch",0l));
+		if (System.currentTimeMillis() - OMWPP.CONFIGJSON.optLong("lastupdateepoch",0l) < 262974l * 60000l) {
+	    	if (OMWPP.DEBUG) Log.i("OMWPP","Config file less than 6 months old - no update.");
+			bNeedRefresh = false;
+		} else {
+			// If last download was more than 6 months ago, then download
+			if (System.currentTimeMillis() - OMWPP.LASTCONFIGREFRESH > 262974l * 60000l) {
+		    	if (OMWPP.DEBUG) Log.i("OMWPP","Config file old, but last DL less than 6 months old - no update.");
+				bNeedRefresh = true;
+			} else {
+		    	if (OMWPP.DEBUG) Log.i("OMWPP","Config file & last DL more than 6 months old - need update.");
+				bNeedRefresh = false;
+			}
+		}
+		
+		if (bNeedRefresh) {
+	    	if (OMWPP.DEBUG) Log.w("OMWPP","WE SHOULD NEVER BE HERE");
 
-		if (!Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
-        	Toast.makeText(this, "SD Card not detected.\nRemember to turn off USB storage if it's still connected!", Toast.LENGTH_LONG).show();
-			finish();
-        	return;
-        }
-		if (!SDROOT.canRead()) {
-        	Toast.makeText(this, "SD Card missing or corrupt.\nRemember to turn off USB storage if it's still connected!", Toast.LENGTH_LONG).show();
-			finish();
-        	return;
-        }
+			AsyncTask<String, Void, String> at = new AsyncTask<String, Void, String>() {
+				@Override
+				protected String doInBackground(String... urls) {
 
+					String response = "";
+					for (String url : urls) {
+						DefaultHttpClient client = new DefaultHttpClient();
+						HttpGet httpGet = new HttpGet(url);
+						try {
+							HttpResponse execute = client.execute(httpGet);
+							JSONObject tempObj = OMWPP.streamToJSONObject(execute.getEntity().getContent());
+							BufferedWriter out = new BufferedWriter(new FileWriter(new File(OMWPP.THUMBNAILROOT.getPath()+ "omwpp_config.json")),8192);
+							out.write(tempObj.toString(5));
+							out.close();
+							OMWPP.PREFS.edit().putLong("lastupdateepoch", System.currentTimeMillis()).commit();
+							OMWPP.LASTCONFIGREFRESH = System.currentTimeMillis();
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+					return response;
+				}
+			};
+			at.execute(new String[]{"http://www.yahoo.com"});
+		}
+	
         setContentView(R.layout.main);
         
         gallery = (Gallery)findViewById(R.id.wpgallery);
         WPPickerAdapter adapter = new WPPickerAdapter();
         //Load all wallpapers in dir into picker.
 //        int counter=0;
-        for (File f : SDROOT.listFiles()) {
+        for (File f : OMWPP.SDROOT.listFiles()) {
         	System.out.println(f.getName());
         	adapter.addItem(f);
         }
@@ -80,6 +120,22 @@ public class OneMoreWallpaperPickerActivity extends Activity {
         btnHelp = (Button)findViewById(R.id.btnhelp);
     }        
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.statusmenu, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		// TODO Auto-generated method stub
+		
+		if (item.getItemId()==R.id.menuforcedownload) {
+			Toast.makeText(OMWPP.CONTEXT, "ForceDownload", Toast.LENGTH_SHORT).show();
+		}
+		return super.onMenuItemSelected(featureId, item);
+	}
+	
     public boolean isConnected() {
     	ConnectivityManager conMgr =  (ConnectivityManager)(this.getSystemService(Context.CONNECTIVITY_SERVICE));
     	boolean result = false;
