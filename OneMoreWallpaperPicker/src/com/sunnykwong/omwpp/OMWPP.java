da68d90744037df3782d17a212de29f82c9455f3
skwong@sunnykwong.com <skwong@sunnykwong.com@3c733f50-8780-0929-1c2b-a13d2c7d3b54>
2012-03-09 15:41:20 +0000

    OMWPP Initial import.

---
+++ OneMoreWallpaperPicker/src/com/sunnykwong/omwpp/OMWPP.java
@@ -0,0 +1,11 @@
+package com.sunnykwong.omwpp;
+
+import android.app.Application;
+
+public class OMWPP extends Application {
+@Override
+public void onCreate() {
+	// TODO Auto-generated method stub
+	super.onCreate();
+}
+}
