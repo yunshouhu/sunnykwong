fcbe18a307aa8b3b423e97e28bbba61c89b7f86f
skwong@sunnykwong.com <skwong@sunnykwong.com@3c733f50-8780-0929-1c2b-a13d2c7d3b54>
2012-03-12 13:28:52 +0000

    Troubleshooting Freezes.

---
+++ trunk/One More Clock/v130_noservice/src/com/sunnykwong/omc/ClockWidget4x1.java
@@ -0,0 +1,79 @@
+package com.sunnykwong.omc;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class ClockWidget4x1 extends AppWidgetProvider {
+	
+	public ClockWidget4x1() {
+		super();
+	}
+
+	//	When one or more widgets are removed...
+	@Override
+	public void onDeleted(Context context, int[] appWidgetIds) {
+		final int N = appWidgetIds.length;
+		for (int i=0; i<N; i++) {
+			//Remove Prefs
+			if (OMC.DEBUG)Log.i(OMC.OMCSHORT + "Widget","Removed Widget #" + appWidgetIds[i]);
+			OMC.removePrefs(appWidgetIds[i]);
+	    	if (OMC.WIDGETBMPMAP.containsKey(appWidgetIds[i])) {
+	    		if (!OMC.WIDGETBMPMAP.get(appWidgetIds[i]).isRecycled()) OMC.WIDGETBMPMAP.get(appWidgetIds[i]).recycle();
+	    		OMC.WIDGETBMPMAP.remove(appWidgetIds[i]);
+	    	}
+		}
+	}
+
+//	When the very last widget is removed.
+	public void onDisabled(Context context) {
+
+		//Flag OMCService to stop.
+		OMCService.STOPNOW4x1=true;
+	}
+		
+//	This gets called when the very first widget is instantiated.
+	public void onEnabled(Context context) {
+
+		//Unflag the STOP FLAG for OMCService.
+		OMCService.STOPNOW4x1=false;
+	}
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		String action = intent.getAction();
+		if (AppWidgetManager.ACTION_APPWIDGET_DELETED.equals(action)) {
+			Bundle extras = intent.getExtras();
+			int appWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID,
+					AppWidgetManager.INVALID_APPWIDGET_ID);
+			if (appWidgetId != AppWidgetManager.INVALID_APPWIDGET_ID) {
+				this.onDeleted(context, new int[] { appWidgetId });
+			}
+		}
+		else {
+			// Set time for output
+	        OMCWidgetDrawEngine.updateAppWidget(context, OMC.WIDGET4x1CNAME);
+
+			super.onReceive(context, intent);
+		}
+	}
+
+	//	This should never fire since I implemented onReceive.
+	@Override
+	public void onUpdate(Context context, AppWidgetManager aWM, int[] appWidgetIds) {
+		if (!OMCService.RUNNING) {
+			OMC.setServiceAlarm(System.currentTimeMillis() + 500);
+		}
+
+		final int N = appWidgetIds.length;
+		for (int i=0; i<N; i++) {
+		  	OMC.initPrefs(appWidgetIds[i]);
+		  	OMCWidgetDrawEngine.updateAppWidget(context, aWM, appWidgetIds[i], OMC.WIDGET4x1CNAME);
+		}
+
+	}
+	
+}
---
+++ trunk/One More Clock/v130_noservice/src/com/sunnykwong/omc/OMCConfigReceiver.java
@@ -0,0 +1,24 @@
+package com.sunnykwong.omc;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+public class OMCConfigReceiver extends BroadcastReceiver {
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		if (OMC.DEBUG) Log.i(OMC.OMCSHORT + "Config","Rcvd " + intent.getAction());
+		String action = intent.getAction();
+		if (action.equals("com.sunnykwong.omc.WIDGET_CONFIG") || action.equals("com.sunnykwong.omc.WIDGET_INDIRECT") ) {
+			Intent intent2 = new Intent(Intent.ACTION_EDIT,intent.getData(),context,OMCPrefActivity.class);
+	        PendingIntent pi = PendingIntent.getActivity(context, 0, intent2, 0);
+	        try {
+	        	pi.send();
+	        } catch (PendingIntent.CanceledException e) {
+	        	e.printStackTrace();
+	        }
+		}
+	}
+}
