9665554a78afe8cd9c3721bb825e2a91eb1897fd
skwong@sunnykwong.com <skwong@sunnykwong.com@3c733f50-8780-0929-1c2b-a13d2c7d3b54>
2014-09-16 05:24:46 +0000

    test

--- One More Clock/src/com/sunnykwong/omc/ClockWidget2x2.java
+++ One More Clock/src/com/sunnykwong/omc/ClockWidget2x2.java
@@ -1,6 +1,6 @@
 package com.sunnykwong.omc;
 
-import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetManager; 
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
